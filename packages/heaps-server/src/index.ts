57e2d7622de62076941fcceae1e25df7758b2a15
Ethan <me@ethandaya.com>
2023-02-13 19:16:19 +1100

    feat: add stub nextjs handler

---
+++ packages/heaps-server/src/index.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+export function heapsNextAdaptor(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method;
+  const { network, event } = req.query;
+  if (method !== "POST") {
+    return res.status(405).end();
+  }
+  console.log("Received event", event, "for network", network);
+  return res.status(200).json({ event, network });
+}
